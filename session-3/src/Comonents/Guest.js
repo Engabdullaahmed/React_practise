6152fedc663d62f949f4f3f21c1e088c1ebb4d9c
Abdulla Ahmed <ahmedmoustafaabdelwahed@gmail.com>
2024-09-03 23:21:49 +0300

    sdflk

--- session-3/src/Comonents/Guest.js
+++
@@ -1,6 +0,0 @@
-function Guest() {
-    return (
-        <h2>SUIIIIII from Guest </h2>
-     )
-}
-export default Guest
--- session-3/src/Comonents/Welcome.js
+++
@@ -1,8 +0,0 @@
-function Welcome(props)
-{
-    return(
-
-        <h2>Wlcome {props.name} </h2>
-    )
-}
-export default Welcome
